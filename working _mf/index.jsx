810448d5a6c89f5a78db107655b059b7675b412c
pranali01-23 <paralepranali@gmail.com>
2024-12-03 13:00:28 +0530

    Update index.jsx

--- working _mf/index.jsx
+++ working _mf/index.jsx
@@ -1,1 +1,26 @@
-aa
+ import React from 'react';
+ //import ReactDOM from 'react-dom';
+ import ReactDOM from 'react-dom/client';
+ //import './styles/App.css';
+ // import './index.css';
+ import App from './App';
+ // import 'bootstrap/dist/css/bootstrap.min.css';
+ import { CssBaseline } from '@mui/material';
+ 
+ // ReactDOM.render(
+ //   <React.StrictMode>
+ //     <CssBaseline />
+ //     <App />
+ //   </React.StrictMode>,
+ //   document.getElementById('root')
+ // );
+ 
+ const root = document.getElementById('root');
+ if (!root) {
+   console.error('React root not found');
+   const p = document.createElement('p');
+   p.textContent = 'React root not found';
+   document.body.appendChild(p);
+ } else {
+   ReactDOM.createRoot(root).render(<App />);
+ }
