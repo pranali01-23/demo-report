9045dc090020f05f6a25e7cdf0c70cb9834c9e3d
pranali01-23 <paralepranali@gmail.com>
2024-12-03 16:23:43 +0530

    Update TestPage.jsx

--- working _mf/new_project/TestPage.jsx
+++ working _mf/new_project/TestPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navbar from './Navbar'; // Import the Navbar component
+import './TestPage.css'; // If you have separate styles for the Test Page
 
 const TestPage = () => {
   return (
     <div>
-      <h2>This is the Test Page</h2>
-      {/* Add additional content for the Test Page */}
+      <Navbar /> {/* Using Navbar component */}
+      <h1>This is the Test Page</h1>
+      {/* Add other content as necessary */}
     </div>
   );
 };
