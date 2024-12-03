1dc736c67b0369dbbdc5b3db0a856cb47b15e898
pranali01-23 <paralepranali@gmail.com>
2024-12-03 14:37:38 +0530

    Create TestPage.jsx

---
+++ working _mf/new_project/TestPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage = () => {
+  return (
+    <div>
+      <h2>This is the Test Page</h2>
+      {/* Add additional content for the Test Page */}
+    </div>
+  );
+};
+
+export default TestPage;
