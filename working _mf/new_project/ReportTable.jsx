11945c7fcb6b6766f37db9fdac46ec0b3dafcfea
pranali01-23 <paralepranali@gmail.com>
2024-12-03 14:39:36 +0530

    Create ReportTable.jsx

---
+++ working _mf/new_project/ReportTable.jsx
@@ -0,0 +1,30 @@
+ import React from 'react';
+ 
+ const ReportTable = ({ reportData }) => {
+   if (!reportData) return null;
+   
+   const keys = Object.keys(reportData[0]||{});
+ 
+   return (
+     <table className="table">
+       <thead>
+         <tr>
+           {keys.map(key => (
+             <th key={key}>{key}</th>
+           ))}
+         </tr>
+       </thead>
+       <tbody>
+         {reportData.map((item, index) => (
+           <tr key={index}>
+             {keys.map(key => (
+               <td key={key}>{item[key]}</td>
+             ))}
+           </tr>
+         ))}
+       </tbody>
+     </table>
+   );
+ };
+ 
+ export default ReportTable;
