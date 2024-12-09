f5017403f5463a57d1537712518f1a44c80fe0b9
pranali01-23 <paralepranali@gmail.com>
2024-12-09 18:32:44 +0530

    Create Loader.jsx

---
+++ working _mf/whole_proj /Loader.jsx
@@ -0,0 +1,14 @@
+// src/Loader.js
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+const Loader = () => {
+    return (
+        <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+            <CircularProgress />
+        </Box>
+    );
+};
+
+export default Loader;
