a4f0e8f2eaa4039ca83748ed3308ef5c17984018
pranali01-23 <paralepranali@gmail.com>
2024-12-09 18:40:14 +0530

    Create App.jsx

---
+++ working _mf/whole_proj /App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ReportPage from './ReportPage';
+
+function App() {
+    return (
+        <div className="App">
+            <ReportPage />
+        </div>
+    );
+}
+
+export default App;
