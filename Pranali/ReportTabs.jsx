11ca86d341815f32c0d692ff1cd5da57316b3bde
pranali01-23 <paralepranali@gmail.com>
2024-12-10 20:26:27 +0530

    Update ReportTabs.jsx

--- Pranali/ReportTabs.jsx
+++ Pranali/ReportTabs.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Tab, Tabs, Typography } from '@mui/material';
+import { Tab, Tabs, Typography, AppBar } from '@mui/material';
 import ReportTab from './ReportTab';
 import DateTimePicker from './DateTimePicker';
+import './ReportTabs.css'; // CSS file for specific tab styling
 
 const ReportTabs = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -17,15 +18,17 @@ const ReportTabs = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <DateTimePicker onSubmit={handleDateTimeSubmit} />
-      <Tabs value={selectedTab} onChange={handleTabChange}>
-        <Tab label="Report 1" />
-        <Tab label="Report 2" />
-        <Tab label="Report 3" />
-        <Tab label="Report 4" />
-        <Tab label="Report 5" />
-      </Tabs>
+      <AppBar position="static">
+        <Tabs value={selectedTab} onChange={handleTabChange}>
+          <Tab label="Report 1" className="tab-label" />
+          <Tab label="Report 2" className="tab-label" />
+          <Tab label="Report 3" className="tab-label" />
+          <Tab label="Report 4" className="tab-label" />
+          <Tab label="Report 5" className="tab-label" />
+        </Tabs>
+      </AppBar>
       <Typography component="div" role="tabpanel">
         <ReportTab reportId={selectedTab} dateTime={dateTime} />
       </Typography>
