51b5708a99da473da5e73a29fe3e8bd2d74755b2
pranali01-23 <paralepranali@gmail.com>
2024-12-10 20:25:43 +0530

    Update App.jsx

--- Pranali/App.jsx
+++ Pranali/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import ReportTabs from './components/ReportTabs';
+import './App.css'; // Import your CSS file
 
 const App = () => {
   return (
     <Container>
-      <h1>Report Viewer</h1>
+      <h1 className="app-title">Report Viewer</h1>
       <ReportTabs />
     </Container>
   );
