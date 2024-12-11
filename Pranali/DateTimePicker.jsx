33de0f4e06e10ed3c49cf3cd694169f484e14460
pranali01-23 <paralepranali@gmail.com>
2024-12-11 15:51:51 +0530

    Update DateTimePicker.jsx

--- Pranali/DateTimePicker.jsx
+++ Pranali/DateTimePicker.jsx
@@ -16,15 +16,37 @@ const DateTimePicker = ({ onSubmit }) => {
     onSubmit(selectedDate);
   };
 
+  // Function to create an array of time options with 30-minute intervals
+  const getTimeOptions = () => {
+    const options = [];
+    const baseDate = new Date();
+    for (let i = 0; i < 48; i++) {
+      const date = new Date(baseDate.setMinutes(baseDate.getMinutes() + 30 * i));
+      options.push(date);
+    }
+    return options;
+  };
+
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <Datetime
         value={selectedDate}
         onChange={handleDateChange}
+        dateFormat="YYYY-MM-DD"
+        timeFormat="HH:mm"
         renderInput={(props) => (
           <TextField {...props} fullWidth label="Select Date & Time" />
         )}
+        // Disable all other times except the options with 30 minutes interval
+        inputProps={{ 
+          list: "time-options" 
+        }}
       />
+      <datalist id="time-options">
+        {getTimeOptions().map((time, index) => (
+          <option key={index} value={time.toISOString()} />
+        ))}
+      </datalist>
       <Button
         variant="contained"
         color="primary"
